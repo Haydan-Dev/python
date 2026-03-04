a2f7bd88c90c62726b85cd9b5ca7d4bfd6c2d952
Haydan Patel <haydan.dev@gmail.com>
2026-03-04 17:05:11 +0530

    new files

--- fileIO-Methods.py
+++ fileIO-Methods.py
@@ -1,12 +1,7 @@
 f = open("for-else.py", "r")
 i = 0
 while True:
-    i = i +1
     line = f.readline()
-    m1 = line.split(",")[0]
-    m2 = line.split(",")[1]
-    m3 = line.split(",")[2]
-    print(f"first line is {i} in ")
     print(line, type(line))
     if not line:
         break
