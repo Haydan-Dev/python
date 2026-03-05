b41cc9a10aebca75404d1c801b474f9955dc13e4
Haydan Patel <haydan.dev@gmail.com>
2026-03-05 17:58:31 +0530

    docs: add script explaining 'is' vs '==' comparison operators

---
+++ isandequaltoequalto.py
@@ -0,0 +1,15 @@
+# in python is keyword is used to compare exact pin point location of an object in memory
+# while on the other hand == symbol is used to compare the value of an object or a variable 
+# Note: -1, both are comparission operators
+a = 4
+b = "4"
+
+
+# checking the types
+print("---------------checkin the types---------")
+print(type(a is b))
+print(type(a==b))
+print("-------------checking the real result---------")
+print(a is b)
+print(a==b)
+
